4b65ffc66499acbe12d7b5ddc08af4c69900451e
jack smith <ahmadi80@gmail.com>
2017-04-12 17:48:27 +0430

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import {RouterModule,Routes} from '@angular/router'
 import { AppComponent } from './app.component';
+import { BlogComponent } from './blog/blog.component';
+import { HomeComponent } from './home/home.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 
+const appRoutes:Routes=[
+  {path:'blog',component:BlogComponent},
+  {path:'home',component:HomeComponent},
+  {path:'',component:HomeComponent},
+  {path:'**',component:PagenotfoundComponent},
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlogComponent,
+    HomeComponent,
+    PagenotfoundComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
